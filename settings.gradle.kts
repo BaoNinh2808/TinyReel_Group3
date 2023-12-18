a96d5bb9c90991e559567e3cd9eb00057f257ad8
BaoNinh2808 <4LegsMonster@gmail.com>
2023-12-18 15:21:32 +0700

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TinyReel"
+include(":app")
+include(":feature")
+include(":feature:home")
+include(":feature:authentication")
+include(":feature:profile")
+include(":feature:post")
+include(":feature:camera")
+include(":common")
+include(":common:composable")
+include(":common:theme")
+include(":core")
+include(":data")
