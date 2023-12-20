764340376f9a3f81e06e3aa25544909e90d38151
BaoNinh2808 <4legsmonster@gmail.com>
2023-12-20 12:28:57 +0700

    common theme

--- README.md
+++ README.md
@@ -13,3 +13,25 @@
 >(ở app của mình là `@Hilt` dùng để Dependency Inject. Để dùng được thì phải implement dependency `com.google.dagger:hilt-android:xxxx` )
 
 - B5: Tìm Activity khởi động - là Activity có nhãn `MAIN` và `LAUNCHER`. 
+
+## Thêm các màu trong file `Theme/Color.kt`
+
+Vì định nghĩa các màu này cần dùng tới hàm `Color()` trong gói `androidx.compose.ui.graphics.Color` nên chúng ta cần phải implement `"androidx.compose.ui:ui:xxxx"` (với xxxx là phiên bản được chọn).
+
+- B1: Vào **file** --> **project structure** --> **dependency** 
+- B2: Chọn module muốn add dependency vào (chọn module `theme`)
+- B3: Nhấn dấu `+`, search `com.google.dagger:hilt-android:*` và chọn phiên bản
+- B4: Nhấn **Apply**
+
+## Thêm font vào file `Theme/Font.kt`
+
+- B1: Tải các font cần thiết về và để trong thư mục **font** ở **res**
+- B2: Tạo file Font.kt giống như trên github
+
+## Thêm các style của chữ vào file `Theme\Type.kt`
+
+Cần imlement `androidx.compose.material3:material3:xxxx`
+
+## Thêm các theme vào file `Theme\theme.kt`
+
+Thêm theme dark và light vào
---
+++ common/theme/src/main/java/com/example/theme/Type.kt
@@ -0,0 +1,82 @@
+package com.example.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    displayMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 27.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Bold
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 17.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleLarge = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold
+    ),
+
+    titleMedium = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    titleSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 15.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fontFamily,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 15.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 13.sp,
+        fontWeight = FontWeight.SemiBold
+
+    ),
+    labelSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+)
