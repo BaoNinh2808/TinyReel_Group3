bf199aa592cb353bb4c77b776b085fe48d911865
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2023-12-21 11:18:01 +0700

    'test'

---
+++ feature/authentication/src/main/java/com/tinyreel/authentication/AuthenticationNavigation.kt
@@ -0,0 +1,15 @@
+//package com.tinyreel.authentication
+//
+//import androidx.navigation.NavController
+//import androidx.navigation.NavGraphBuilder
+//import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+//import com.google.accompanist.navigation.material.bottomSheet
+//import com.puskal.core.DestinationRoute.AUTHENTICATION_ROUTE
+//
+//
+//@OptIn(ExperimentalMaterialNavigationApi::class)
+//fun NavGraphBuilder.authenticationNavGraph(navController: NavController) {
+//    bottomSheet(route = AUTHENTICATION_ROUTE) {
+//        AuthenticationScreen(navController)
+//    }
+//}
