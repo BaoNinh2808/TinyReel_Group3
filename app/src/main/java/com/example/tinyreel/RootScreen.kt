17b3a59d96d7f356df547ec7745d0a5ce8ba873e
ndtduy <ndtduy061003@gmail.com>
2023-12-22 21:21:35 +0700

    Root screen components

--- app/src/main/java/com/example/tinyreel/RootScreen.kt
+++ app/src/main/java/com/example/tinyreel/RootScreen.kt
@@ -3,11 +3,23 @@ package com.example.tinyreel
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material.SwipeableDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -25,6 +37,10 @@ import com.example.core.DestinationRoute.HOME_SCREEN_ROUTE
 import com.example.core.DestinationRoute.INBOX_ROUTE
 import com.example.core.DestinationRoute.MY_PROFILE_ROUTE
 import com.example.theme.TinyReelTheme
+import com.example.tinyreel.component.BottomBar
+import com.example.tinyreel.navigation.AppNavHost
+
+//import com.example.tinyreel.navigation.AppNavHost
 
 @OptIn(
     ExperimentalMaterial3Api::class, ExperimentalMaterialNavigationApi::class,
@@ -94,7 +110,7 @@ fun SetupSystemUi(
 }
 
 @ExperimentalMaterialNavigationApi
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun rememberBottomSheetNavigator(
     animationSpec: AnimationSpec<Float> = SwipeableDefaults.AnimationSpec,
