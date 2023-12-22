ce792294c86ca089debbed739a77027c5d8ee7ab
BaoNinh2808 <4legsmonster@gmail.com>
2023-12-22 21:40:36 +0700

    new rootscreen

--- app/src/main/java/com/example/tinyreel/RootScreen.kt
+++ app/src/main/java/com/example/tinyreel/RootScreen.kt
@@ -3,14 +3,6 @@ package com.example.tinyreel
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.material.SwipeableDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
