50b5fde9933097dd680e2bd23129c1a85fe6c5aa
BaoNinh2808 <4legsmonster@gmail.com>
2023-12-22 20:37:57 +0700

    dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation(project(":core"))
     implementation(project(":feature:home"))
+    implementation("com.google.accompanist:accompanist-adaptive:0.33.2-alpha")
+    implementation("com.google.accompanist:accompanist-permissions:0.33.2-alpha")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/tinyreel/RootScreen.kt
+++ app/src/main/java/com/example/tinyreel/RootScreen.kt
@@ -1,27 +1,106 @@
 package com.example.tinyreel
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import android.app.Activity
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.home.HomeScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.core.DestinationRoute.AUTHENTICATION_ROUTE
+import com.example.core.DestinationRoute.CAMERA_ROUTE
+import com.example.core.DestinationRoute.COMMENT_BOTTOM_SHEET_ROUTE
+import com.example.core.DestinationRoute.FORMATTED_COMPLETE_CREATOR_VIDEO_ROUTE
+import com.example.core.DestinationRoute.FRIENDS_ROUTE
+import com.example.core.DestinationRoute.HOME_SCREEN_ROUTE
+import com.example.core.DestinationRoute.INBOX_ROUTE
+import com.example.core.DestinationRoute.MY_PROFILE_ROUTE
+import com.example.theme.TinyReelTheme
 
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalMaterialNavigationApi::class,
+    ExperimentalAnimationApi::class
+)
 @Composable
-fun RootScreen(){
+fun RootScreen() {
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    val navController = rememberNavController(bottomSheetNavigator)
+    val currentBackStackEntryAsState by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntryAsState?.destination
+    val context= LocalContext.current
 
-    //Vd ve 1 man hinh
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(10.dp)
-        ) {
-            Text(text = "AAAAAA")
-            Text(text = "BBBBBB")
+
+    val isShowBottomBar = when (currentDestination?.route) {
+        HOME_SCREEN_ROUTE, INBOX_ROUTE, COMMENT_BOTTOM_SHEET_ROUTE,
+        FRIENDS_ROUTE, AUTHENTICATION_ROUTE, MY_PROFILE_ROUTE, null -> true
+        else -> false
+    }
+    val darkMode = when (currentDestination?.route) {
+        HOME_SCREEN_ROUTE, FORMATTED_COMPLETE_CREATOR_VIDEO_ROUTE, CAMERA_ROUTE, null -> true
+        else -> false
+    }
+
+    if(currentDestination?.route== HOME_SCREEN_ROUTE){
+        BackHandler {
+            (context as? Activity)?.finish()
+        }
+    }
+
+    TinyReelTheme(darkTheme = darkMode) {
+        SetupSystemUi(rememberSystemUiController(), MaterialTheme.colorScheme.background)
+        ModalBottomSheetLayout(bottomSheetNavigator = bottomSheetNavigator) {
+            Scaffold(
+                topBar = {
+
+                },
+                bottomBar = {
+                    if (!isShowBottomBar) {
+                        return@Scaffold
+                    }
+                    BottomBar(navController, currentDestination, isDarkTheme = darkMode)
+                }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                ) {
+                    AppNavHost(navController = navController)
+                }
+            }
         }
 
-    HomeScreen()
+
+    }
+}
+
+@Composable
+fun SetupSystemUi(
+    systemUiController: SystemUiController,
+    systemBarColor: Color
+) {
+    SideEffect {
+        systemUiController.setSystemBarsColor(color = systemBarColor)
+    }
+}
+
+@ExperimentalMaterialNavigationApi
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun rememberBottomSheetNavigator(
+    animationSpec: AnimationSpec<Float> = SwipeableDefaults.AnimationSpec,
+    skipHalfExpanded: Boolean = true,
+): BottomSheetNavigator {
+    val sheetState = rememberModalBottomSheetState(
+        ModalBottomSheetValue.Hidden,
+        animationSpec,
+        skipHalfExpanded,
+    )
+    return remember(sheetState) {
+        BottomSheetNavigator(sheetState = sheetState)
+    }
 }
