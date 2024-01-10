98837d1c9efb4e8ec5817445e33cb508cfed106a
ezio-noir <ezionoir@outlook.com>
2024-01-10 16:16:14 +0700

    merged with my profile

---
+++ feature/myprofile/src/main/java/com/example/myprofile/MyProfileViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.myprofile
+
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.example.core.DestinationRoute
+import com.example.core.base.BaseViewModel
+import com.example.creatorprofile.screen.creatorprofile.CreatorProfileEvent
+import com.example.creatorprofile.screen.creatorprofile.ViewState
+import com.example.data.model.UserModel
+import com.example.data.model.VideoModel
+import com.example.domain.creatorprofile.GetCreatorProfileUseCase
+import com.example.domain.creatorprofile.GetCreatorPublicVideoUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyProfileViewModel
+@Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    public val getCreatorProfileUseCase: GetCreatorProfileUseCase,
+    private val getCreatorPublicVideoUseCase: GetCreatorPublicVideoUseCase
+) : BaseViewModel<ViewState, CreatorProfileEvent>() {
+    val userId: Long? = savedStateHandle[DestinationRoute.PassedKey.USER_ID]
+
+    private val _publicVideosList = MutableStateFlow<List<VideoModel>>(arrayListOf())
+    val publicVideosList = _publicVideosList.asStateFlow()
+
+    private val _likedVideosList = MutableStateFlow<List<VideoModel>>(arrayListOf())
+    val likedVideosList = _likedVideosList.asStateFlow()
+
+    override fun onTriggerEvent(event: CreatorProfileEvent) {
+    }
+
+    init {
+        userId?.let {
+            fetchUser(it)
+            fetchCreatorPublicVideo(it)
+        }
+    }
+
+    fun fetchUser(id: Long) {
+        viewModelScope.launch {
+            getCreatorProfileUseCase(id).collect {
+                updateState(ViewState(creatorProfile = it))
+            }
+        }
+    }
+
+    fun getUserModel(): UserModel? {
+        return viewState.value?.creatorProfile
+    }
+
+    fun updateUserName(newValue: String) {
+        viewState.value?.creatorProfile?.let {userModel ->
+            val updatedUserModel = userModel.copy(uniqueUserName = newValue)
+            updateState(ViewState(creatorProfile = updatedUserModel))
+        }
+    }
+
+    private fun fetchCreatorPublicVideo(id: Long) {
+        viewModelScope.launch {
+            getCreatorPublicVideoUseCase(id).collect {
+                Log.d("d", "my video si ${it}")
+                _publicVideosList.value = it
+            }
+        }
+    }
+
+}
